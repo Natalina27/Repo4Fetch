2a83e8e55d66d9a78d45fa93b5f63010c825dde8
natalyamyunster <natalyamyunster@yahoo.com>
2021-03-22 14:30:56 +0100

    nm

--- app.js
+++ app.js
@@ -1,58 +1,46 @@
 const userPhoto = document.querySelector('.user__photo');
 const userName = document.querySelector('.user__nickname');
 const userBio = document.querySelector('.user__description');
-const url = window.location.search;
-const params = new URLSearchParams(url);
-// console.log(params.get("name"));
-// console.log("url", url);
+const url = 'https://api.github.com/users/';
 
 let nickName = prompt('Enter Your NickName', 'Icreateacoolname');
 if (!nickName) {
-    nickName = 'Icreateacoolname';
+  nickName = 'Icreateacoolname';
 }
 
-
-// let getnickName = params.get("name");
-
-const delay = ms => {
-  return new Promise((resolve) => {
+const delay = ms => new Promise((resolve) => {
     setTimeout(() => {
       resolve();
     }, ms);
-  })
-}
+  });
 
 async function getResponse() {
   await delay(2000);
-  let response = await fetch(`https://api.github.com/users/${nickName}`);
+  const response = await fetch(`${url}${nickName}`);
+
   return response.json();
 } //получает запров в виде json файла
 
 async function getUserData() {
   try {
     const content = await getResponse();
+    console.log('content',content);
+    const{name, bio, avatar_url,html_url } = content;
 
-    if (content.name) {
-      userName.append(content.name);
-    }else {
-      userName.append(' ' + 'Информация о пользователе недоступна!');
-    }
-
-    if (content.bio) {
-      userBio.append(content.bio);
-    }else {
-      userBio.append(' ' + 'Информация о пользователе недоступна!');
-    }
-
+    userName.append(name);
+    userBio.append(bio);
     let photo = new Image(300, 400);
-    photo.src = content.avatar_url;
+    photo.src = avatar_url;
     userPhoto.append(photo);
 
-    let link = content.html_url;
+    let link = html_url;
     userName.onclick = () => window.location.assign(link);
     userName.style.cursor = 'pointer';
+
+    return { name, bio, avatar_url,html_url};
   }
-  catch {
+  catch(error) {
+    userBio.append(error.message);
     console.error(error);
   }
 }//дожидается конца работы getResponse и работает с данными
@@ -60,8 +48,15 @@ async function getUserData() {
 async function getDate() {
   await delay(2000);
   console.log (new Date());
+  return new Date();
 }
 
-getUserData();
-getDate();
+Promise.all([getUserData(), getDate()])
+    .then((result) =>{
+      console.log(result);
+    })
+
+// getUserData();
+// getDate();
+
 
